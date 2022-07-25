f7adcd6459cace0d570cd147eccaabd1e6eab302
HelHitch <olena9002@gmail.com>
2022-07-25 15:01:45 +0400

    .

--- js/script.js
+++ js/script.js
@@ -1,24 +1,45 @@
 //slick-slider
 
-$(document).on("ready", function () {
-  $(".watch__inner").slick({
-    slidesToScroll: 1,
-    infinite: true,
+$(document).ready(function () {
+  $(".watch__inner").flickity({
+    // options
+    cellAlign: "center",
+    contain: true,
     adaptiveHeight: true,
-    prevArrow:
-      '<button type="button" class="slick-prev"><img src="img/icons/left.svg" /> </button>',
-    nextArrow:
-      '<button type="button" class="slick-next"><img src="img/icons/right.svg" /></button>',
-
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          dots: true,
-          prevArrow: '<button type="button" class="slick-prev"> </button>',
-          nextArrow: '<button type="button" class="slick-next"></button>',
-        },
-      },
-    ],
   });
+
+  //функция переключения табов
+  $("ul.catalog__tabs").on(
+    "click",
+    "li:not(.catalog__tab_active)",
+    function () {
+      $(this)
+        .addClass("catalog__tab_active")
+        .siblings()
+        .removeClass("catalog__tab_active")
+        .closest("div.container")
+        .find("div.catalog__content")
+        .removeClass("catalog__content_active")
+        .eq($(this).index())
+        .addClass("catalog__content_active");
+    }
+  );
+
+  //функция переключения слайдера
+  function toggleClass(item) {
+    $(item).each(function (i) {
+      $(this).on("click", function (e) {
+        e.preventDefault();
+        $(".catalog-element__main")
+          .eq(i)
+          .toggleClass("catalog-element__main_active");
+        $(".catalog-element__additional")
+          .eq(i)
+          .toggleClass("catalog-element__additional_active");
+      });
+    });
+  }
+
+  toggleClass(".catalog-element__link");
+  toggleClass(".catalog-element__link_back");
 });
